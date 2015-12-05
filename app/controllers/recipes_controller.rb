184298865754e32c50fae3959e54b7cc537b4337
Brien Crean <brien.crean@gmail.com>
2015-12-05 14:57:58 -0800

    added very basic CRUD for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,9 @@
 class RecipesController < ApplicationController
 
-  def index
+  before_action :find_recipe, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @recipes = Recipe.order("created_at")
   end
 
   def new
@@ -10,12 +12,44 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    redirect_to recipes_path
   end
 
   private
 
   def recipe_params
       params.require(:recipe).permit([:title, :description, :cook_time, :prep_time])
+  end
+
+  def find_recipe
+    @recipe = Recipe.find params[:id]
   end
 
 end
