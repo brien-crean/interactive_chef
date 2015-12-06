1f7c36b006611639d848aecc6027511b2df96de8
Brien Crean <brien.crean@gmail.com>
2015-12-06 15:31:38 -0800

    added sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+
+  def user_signed_in?
+    current_user.present?
+  end
+  helper_method :user_signed_in?
+
+  def authenticate_user
+    unless user_signed_in?
+      redirect_to new_session_path, alert: "Please sign in!"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   root 'recipes#index'
   resources :recipes
   resources :users, only: [:new, :create]
+
+  resources :sessions, only: [:new, :create] do
+    delete :destroy, on: :collection
+  end
+
 end
